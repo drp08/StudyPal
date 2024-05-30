b60a51022ec24416cbcceafb447e1f84e90c4322
Nishant Aanjaney Jalan <cybercoder.nishant@gmail.com>
2024-05-30 18:12:47 +0100

    fix: server listens to POST /schedule

--- server/src/main/kotlin/io/github/drp08/studypal/ScheduleRoute.kt
+++ server/src/main/kotlin/io/github/drp08/studypal/ScheduleRoute.kt
@@ -1,31 +1,27 @@
 package io.github.drp08.studypal
 
-import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.call
-import io.ktor.server.request.receive
+import io.ktor.server.request.receiveText
 import io.ktor.server.response.respond
 import io.ktor.server.response.respondText
 import io.ktor.server.routing.Route
 import io.ktor.server.routing.get
 import io.ktor.server.routing.post
-import io.ktor.server.routing.route
 import java.time.LocalTime
 
 fun Route.scheduleRouting() {
-    route("/schedule") {
-        get("name") {
-            val name = call.parameters["name"]
-            val subject = Subject(
-                name = name!!,
-                startTime = LocalTime.of(22,0,0).toSecondOfDay(),
-                endTime = LocalTime.of(23,0,0).toSecondOfDay(),
-                noTotalSessions = 6
-            )
-            call.respond(subject)
-        }
-        post {
-            val subject = call.receive<Subject>()
-            call.respondText("Subject created", status = HttpStatusCode.Created)
-        }
+    get("/") {
+        call.respondText("Hello world!")
+    }
+    post("/schedule") {
+        val subjectName = call.receiveText()
+        val subject = Subject(
+            name = subjectName,
+            startTime = LocalTime.of(22, 0, 0).toSecondOfDay(),
+            endTime = LocalTime.of(23, 0, 0).toSecondOfDay(),
+            noTotalSessions = 6
+        )
+
+        call.respond(subject)
     }
 }
