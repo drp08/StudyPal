9e685ea90c87992799a7cb75bbf003cfe90721c8
Nishant Aanjaney Jalan <cybercoder.nishant@gmail.com>
2024-05-30 15:34:43 +0100

    feat: more buttons and text to the screen

--- composeApp/src/commonMain/kotlin/io/github/drp08/studypal/screens/PomodoroScreen.kt
+++ composeApp/src/commonMain/kotlin/io/github/drp08/studypal/screens/PomodoroScreen.kt
@@ -1,10 +1,17 @@
 package io.github.drp08.studypal.screens
 
 import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.Button
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.Icon
@@ -20,6 +27,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -46,37 +54,52 @@ object PomodoroScreen : Screen {
         Scaffold(
             modifier = Modifier
                 .fillMaxSize(),
-            floatingActionButton = {
-                FloatingActionButton(
-                    onClick = {}
-                ) {
-                    Icon(Icons.Default.Add, contentDescription = null)
-                }
-            }
-        ) {
+        ) { paddingValues ->
             Box(
                 modifier = Modifier
                     .fillMaxSize()
+                    .padding(paddingValues)
             ) {
-                Box(
+                Column(
                     modifier = Modifier
+                        .fillMaxWidth()
                         .align(Alignment.Center)
-                        .fillMaxWidth(),
-                    contentAlignment = Alignment.Center
                 ) {
-                    CircularProgressIndicator(
+                    Row(
                         modifier = Modifier
-                            .fillMaxWidth(0.5f)
-                            .aspectRatio(1f),
-                        progress = (animatedCurrent / from),
-                        strokeWidth = 12.dp,
-                        strokeCap = StrokeCap.Round
-                    )
+                            .padding(horizontal = 16.dp)
+                            .fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        Button(onClick = {}) {
+                            Text(text = "Pomodoro short break")
+                        }
+                        Spacer(modifier = Modifier.width(24.dp))
+                        Button(onClick = {}) {
+                            Text(text = "Pomodoro long break")
+                        }
+                    }
+                    Text(text = "Session finishes at 9:30", modifier = Modifier.padding(vertical = 16.dp).fillMaxWidth(), textAlign = TextAlign.Center)
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        CircularProgressIndicator(
+                            modifier = Modifier
+                                .fillMaxWidth(0.5f)
+                                .aspectRatio(1f),
+                            progress = (animatedCurrent / from),
+                            strokeWidth = 12.dp,
+                            strokeCap = StrokeCap.Round
+                        )
 
-                    Text(
-                        text = current.toString(),
-                        fontSize = 32.sp
-                    )
+                        Text(
+                            text = current.toString(),
+                            fontSize = 32.sp
+                        )
+                    }
+                    Text(text = "Until Break", modifier = Modifier.padding(vertical = 16.dp).fillMaxWidth(), textAlign = TextAlign.Center)
                 }
             }
         }
