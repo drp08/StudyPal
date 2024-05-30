02017e3ce3efa663a5af399c6090ba31a2a1fae9
Nishant Aanjaney Jalan <cybercoder.nishant@gmail.com>
2024-05-31 00:28:52 +0100

    feat: home screen UI (#12)
    * feat: added upcoming events as scrollable widget
    
    * feat: added next session widget and amended structure
    
    * feat: added navigation to new screen when session is clicked
    
    * feat: added info about next event and session ids for upcoming events
    
    * feat: first box navigates to pomodoro
    
    * feat: migrated state logic to viewmodel and fixed response parsing
    
    * refactor: conditionally show items on the screen
    
    * fix: randomly select a subject
    
    * fix: conditionally show checkin button
    
    ---------
    
    Co-authored-by: Harini-Sritharar <harinisri2004@gmail.com>

--- server/src/main/kotlin/io/github/drp08/studypal/Application.kt
+++ server/src/main/kotlin/io/github/drp08/studypal/Application.kt
@@ -1,6 +1,9 @@
 package io.github.drp08.studypal
 
 import io.github.drp08.studypal.routes.scheduleRouting
+import io.github.drp08.studypal.utils.Constants
+import io.ktor.http.ContentType
+import io.ktor.serialization.kotlinx.KotlinxSerializationConverter
 import io.ktor.serialization.kotlinx.json.json
 import io.ktor.server.application.Application
 import io.ktor.server.application.install
@@ -9,12 +12,13 @@ import io.ktor.server.netty.Netty
 import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.server.resources.Resources
 import io.ktor.server.routing.routing
+import kotlinx.serialization.json.Json
 
 fun main() {
     embeddedServer(
         Netty,
-        port = 8080,
-        host = "0.0.0.0",
+        port = Constants.SERVER_PORT,
+        host = Constants.SERVER_HOST,
         module = Application::module
     ).start(wait = true)
 }
--- shared/src/commonMain/kotlin/io/github/drp08/studypal/di/AppModule.kt
+++ shared/src/commonMain/kotlin/io/github/drp08/studypal/di/AppModule.kt
@@ -2,17 +2,23 @@ package io.github.drp08.studypal.di
 
 import io.github.drp08.studypal.data.SchedulingRepositoryImpl
 import io.github.drp08.studypal.domain.SchedulingRepository
+import io.github.drp08.studypal.utils.Constants
 import io.ktor.client.HttpClient
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.plugins.defaultRequest
 import io.ktor.client.plugins.resources.Resources
 import io.ktor.http.URLProtocol
+import io.ktor.serialization.kotlinx.json.json
 
 object AppModule {
     val httpClient: HttpClient = HttpClient {
         install(Resources)
+        install(ContentNegotiation) {
+            json()
+        }
         defaultRequest {
-            host = "0.0.0.0"
-            port = 8080
+            host = Constants.SERVER_HOST
+            port = Constants.SERVER_PORT
             url {
                 protocol = URLProtocol.HTTP
             }
