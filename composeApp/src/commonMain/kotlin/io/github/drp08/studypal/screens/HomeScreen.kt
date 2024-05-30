005a9790ee6db8fe844afa499dddd1133172c1ab
Harini-Sritharar <harinisri2004@gmail.com>
2024-05-30 20:33:50 +0100

    feat: added navigation to new screen when session is clicked

--- composeApp/src/commonMain/kotlin/io/github/drp08/studypal/screens/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/io/github/drp08/studypal/screens/HomeScreen.kt
@@ -1,6 +1,7 @@
 package io.github.drp08.studypal.screens
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.gestures.draggable
 import androidx.compose.foundation.gestures.rememberDraggableState
@@ -9,7 +10,6 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
@@ -22,6 +22,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.core.screen.Screen
+import cafe.adriel.voyager.navigator.LocalNavigator
+import cafe.adriel.voyager.navigator.currentOrThrow
 import kotlinx.coroutines.launch
 
 object HomeScreen : Screen {
@@ -29,23 +31,28 @@ object HomeScreen : Screen {
     override fun Content() {
         val BLUE = Color(0xFF98A9D1)
         val PINK = Color(0xFFFFD3D3)
+        val navigator = LocalNavigator.currentOrThrow
         // Widget for current session/ next session
         Box {
-            Column(modifier = Modifier.fillMaxWidth().wrapContentSize(align = Alignment.TopCenter).padding(vertical = 10.dp).background(color = BLUE)) {
-                Text("Next Session starting in:")
-                Text("00:32:17")
-            }
+                Column(
+                    modifier = Modifier.fillMaxWidth()
+                        .wrapContentSize(align = Alignment.TopCenter)
+                        .padding(vertical = 10.dp).background(color = BLUE)
+                        .clickable(onClick = {navigator.push(ProfileScreen)}) // will route to the Pomodoro screen once completed
+                ) {
+                    Text("Next Session starting in: 00:32:17")
+                }
         }
         // Widget for upcoming sessions
         Box {
             val scrollState = rememberLazyListState()
             val coroutineScope = rememberCoroutineScope()
-            Column(modifier = Modifier.fillMaxWidth().wrapContentSize(align = Alignment.Center).padding(vertical = 75.dp)) {
-            Text(text = "Upcoming sessions/events today")
+            Column {
+            Text(text = "Upcoming sessions/events today",modifier = Modifier.fillMaxWidth().wrapContentSize(align = Alignment.Center).padding(vertical = 70.dp))
                 LazyColumn(
                     state = scrollState,
-                    modifier = Modifier
-                        .wrapContentHeight(align = Alignment.CenterVertically)
+                    modifier = Modifier.fillMaxWidth()
+                        .wrapContentSize(align = Alignment.Center)
                         .draggable(
                             orientation = Orientation.Vertical,
                             state = rememberDraggableState { delta ->
@@ -60,20 +67,23 @@ object HomeScreen : Screen {
                             modifier = Modifier
                                 .wrapContentSize(align = Alignment.Center)
                         ) {
-                            Column(
-                                modifier = Modifier.padding(15.dp)
-                                    .background(color = PINK)
-                            ) {
-                                Text("10:00 - 12:00")
-                                Text("Subject $it")
+                            Column {
+                                Column(
+                                    modifier = Modifier.padding(15.dp)
+                                        .background(color = PINK)
+                                ) {
+                                    Text("10:00 - 12:00")
+                                    Text("Subject ${it + 1}")
+                                }
+                                Column(
+                                    modifier = Modifier.padding(15.dp)
+                                        .background(color = BLUE)
+                                ) {
+                                    Text("13:00 - 15:00")
+                                    Text("Subject ${5 - it}")
+                                }
                             }
-                            Column(
-                                modifier = Modifier.padding(75.dp)
-                                    .background(color = BLUE)
-                            ) {
-                                Text("12:00 - 14:00")
-                                Text("Subject $it")
-                            }
+
                         }
                     }
                 }
