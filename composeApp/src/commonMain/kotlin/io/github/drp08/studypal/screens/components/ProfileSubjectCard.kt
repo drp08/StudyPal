9f47b28bd639666fbc47a03e5621aa61473cc522
Nishant Aanjaney Jalan <cybercoder.nishant@gmail.com>
2024-06-03 16:41:20 +0100

    feat: added an expandable section that doesn't function

--- composeApp/src/commonMain/kotlin/io/github/drp08/studypal/screens/components/ProfileSubjectCard.kt
+++ composeApp/src/commonMain/kotlin/io/github/drp08/studypal/screens/components/ProfileSubjectCard.kt
@@ -1,21 +1,29 @@
 package io.github.drp08.studypal.screens.components
 
 import androidx.annotation.FloatRange
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.Card
+import androidx.compose.material.Icon
 import androidx.compose.material.LinearProgressIndicator
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.unit.dp
 import kotlinx.datetime.Instant
@@ -56,7 +64,27 @@ fun ProfileSubjectCard(
                 Text(text = "Exam Date: $it")
             }
 
-            // TODO Add expandable panel
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .border(width = 1.dp, color = Color.Black),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(text = "Expand")
+
+                Box(
+                    modifier = Modifier
+                        .height(32.dp)
+                        .aspectRatio(1f)
+                        .border(width = 1.dp, color = Color.Black)
+                ) {
+                    Icon(
+                        Icons.Default.ArrowDropDown,
+                        contentDescription = null,
+                        modifier = Modifier.fillMaxSize()
+                    )
+                }
+            }
         }
     }
 }
